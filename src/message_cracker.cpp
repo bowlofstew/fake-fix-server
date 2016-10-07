2e877b1f73b20cb5c37f0132f361ee28cf1368d8
Stewart Henderson <stewart.henderson@protonmail.com>
2016-10-06 19:47:18 -0500

    updates

--- src/message_cracker.cpp
+++
@@ -1,6 +0,0 @@
-#include "message_cracker.hpp"
-
-namespace bowlofstew {
-void message_cracker::onMessage(const FIX44::MarketDataRequest& request,
-                                const FIX::SessionID& id) {}
-}
